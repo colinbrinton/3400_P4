e52f74d7479e98fc0c4d8fdcf02d4e0150bc199c
scienceguy12 <colinbrinton@gmail.com>
2016-05-06 05:05:56 -0700

    working

--- P4.py
+++ P4.py
@@ -1,11 +1,37 @@
 import re
 import sys
+from itertools import cycle, count
 
 FILE = 1
 INVALID_EXT1 = '.exe'
 INVALID_EXT2 = '.bat'
 REPLACE_ED = 'd'
 REPLACE_ER = 'xor'
+REPLACE_F = 'ph'
+REPLACE_A = '&'
+L33T_LIST = ['d00d', 'w00t', 'sk1llz', 'h4xx', 'n0sc0p3', 'pwn4g3', 'r0f1']
+A = 2.5
+B = 2.5
+C = 5
+SEED = 1
+INCREMENT = 1
+WORD_DELIM = ' '
+LINE_DELIM = '\n'
+OUTPUT = 'n00b.txt'
+
+
+def extract_l33t():
+    repeat_l33t = cycle(L33T_LIST)
+    for word in repeat_l33t:
+        yield word
+
+
+def generate_int():
+    n = SEED
+    while n:
+        yield int(A*n*n + B*n + C)
+        n += INCREMENT
+
 
 try:
     filename = sys.argv[FILE]
@@ -30,17 +56,44 @@ if valid_filename.group('extension') in invalid_extensions:
         print(ext)
     sys.exit()
 
-f = open(filename, 'r')
-string = f.read()
-f.close()
-
 process_ed = re.compile(r'''ed  # Match the characters 'ed'
-    \b  # Match only at the end of a word boundary''', re.X)
+                            \b  # Match only at the end of a word boundary''', re.X)
 
 process_er = re.compile(r'''er  # Match the characters 'er'
                             \b  # Match only at the end of a word boundary''', re.X)
 
-replaced_ed = process_ed.sub(REPLACE_ED, string)
-replaced_er = process_er.sub(REPLACE_ER, replaced_ed)
+process_a = re.compile(r'''(and|ant|anned) #Match the three substrings anywhere in string
+                                ''', re.X)
 
-print(replaced_er)
+process_f = re.compile(r'''\b   # Match at the beginning of a word boundary
+                            f   # Match for the character 'f' ''', re.X)
+
+f = open(filename, 'r')
+o = open(OUTPUT, 'w')
+
+word_count = 0
+insert = generate_int()
+w0rd = extract_l33t()
+index = next(insert)
+
+for line in f:
+    replaced_ed = process_ed.sub(REPLACE_ED, line)
+    replaced_er = process_er.sub(REPLACE_ER, replaced_ed)
+    replaced_a = process_a.sub(REPLACE_A, replaced_er)
+    replaced_f = process_f.sub(REPLACE_F, replaced_a)
+
+    split_string = replaced_f.split()
+    line_len = len(split_string)
+    print(line_len)
+    word_count += line_len
+    print(word_count)
+    if word_count >= index:
+        split_string.insert(index, next(w0rd))
+        index = next(insert)
+
+    output_line = WORD_DELIM.join(split_string)
+    o.write(output_line)
+    o.write(LINE_DELIM)
+
+f.close()
+o.close()
